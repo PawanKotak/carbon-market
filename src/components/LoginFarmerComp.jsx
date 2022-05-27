4d620f94cb42002268d8054b5a2d71cf5e6bebaa
Pawan Kotak <kotakpawan@gmail.com>
2022-05-27 12:47:43 +0530

    form control alignment

--- src/components/LoginFarmerComp.jsx
+++ src/components/LoginFarmerComp.jsx
@@ -12,7 +12,7 @@ const LoginFarmerComp = () => {
   const [loaded, setLoaded] = useState(true);
   const { authenticate, isAuthenticated, user } = useMoralis();
   const [ipfsData, setIPFSData] = useState();
-  const [cc,setCC] = useState(0);
+  const [cc, setCC] = useState(0);
 
   // if (!isAuthenticated) {
   //   return (
@@ -26,9 +26,9 @@ const LoginFarmerComp = () => {
     console.log("TEST");
     setLoaded(false);
     const inputData = {
-      text: `CC:${cc} T:${new Date().getTime()}`
-    }
-    axios.post(BACKEND_URL + "/getImage",inputData, config).then((resp) => {
+      text: `CC:${cc} T:${new Date().getTime()}`,
+    };
+    axios.post(BACKEND_URL + "/getImage", inputData, config).then((resp) => {
       const imageURL = resp.data.imageURL;
       setNFTImage(imageURL + "?" + Math.random());
       setLoaded(true);
@@ -37,94 +37,111 @@ const LoginFarmerComp = () => {
 
   const submitHandler = (event) => {
     console.log(`Submit Handler`, event.target.elements);
-    const {carbonpoints,mobile,name, aadharnum, state,gender,imageFile,city,price,country} = event.target.elements;
-    console.log(`Carbonpoint`,carbonpoints.value);
+    const {
+      carbonpoints,
+      mobile,
+      name,
+      aadharnum,
+      state,
+      gender,
+      imageFile,
+      city,
+      price,
+      country,
+    } = event.target.elements;
+    console.log(`Carbonpoint`, carbonpoints.value);
     event.preventDefault();
     let formdata = {
-              carbonpoints:carbonpoints.value,
-              mobile:mobile.value,
-              name:name.value,
-              aadharnum:aadharnum.value,
-              city:city.value,
-              state:state.value,
-              country:country.value,
-              price:price.value,
-              gender:gender.value
-             }
-   
+      carbonpoints: carbonpoints.value,
+      mobile: mobile.value,
+      name: name.value,
+      aadharnum: aadharnum.value,
+      city: city.value,
+      state: state.value,
+      country: country.value,
+      price: price.value,
+      gender: gender.value,
+    };
+
     console.table(formdata);
-    axios.post(BACKEND_URL,formdata,config).then((data) => {
+    axios.post(BACKEND_URL, formdata, config).then((data) => {
       setIPFSData(data);
     });
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="form-group">
-        <label forHTML="carbonpoints"> Carbon Credit</label>
-        <input
-          className="form-control"
-          id="carbonpoints"
-          type="number"
-          placeholder="Enter carbon credit"
-          onChange={(e)=>setCC(e.target.value)}
-        ></input>
+    <form onSubmit={submitHandler} >
+      <div className="row">
+        <div className="form-group col-4">
+          <label forHTML="carbonpoints"> CARBON CREDIT</label>
+          <input
+            className="form-control"
+            id="carbonpoints"
+            type="number"
+            placeholder="Enter carbon credit"
+            onChange={(e) => setCC(e.target.value)}
+          ></input>
+        </div>
+        <div className="form-group col-4">
+          <label>PRICE</label>
+          <input className="form-control" id="price" type="number"></input>
+        </div>
       </div>
-     
-      <div className="form-group">
-        <label>Name</label>
-        <input
-          className="form-control"
-          id="name"
-          placeholder="Enter name"
-        ></input>
+      <div className="row">
+        <div className="form-group col-4">
+          <label>NAME</label>
+          <input
+            className="form-control"
+            id="name"
+            placeholder="Enter name"
+          ></input>
+        </div>
+        <div className="form-group col-4">
+          <label> MOBILE</label>
+          <input
+            className="form-control"
+            id="mobile"
+            placeholder="Enter phone number"
+            type="tel"
+          ></input>
+        </div>
+        <div className="form-group col-4">
+          <label>AADHAR CARD NUMBER</label>
+          <input className="form-control" id="aadharnum"></input>
+        </div>
+
+        <div className="form-group col-4">
+          <label>CITY</label>
+          <input className="form-control" id="city"></input>
+        </div>
+        <div className="form-group col-4">
+          <label>STATE</label>
+          <select className="form-control" id="state">
+            <option disabled="true">---Select State----</option>
+            <option>ANDHRA PRADESH</option>
+            <option>ASSAM</option>
+            <option>BIHAR</option>
+            <option>CHANDIGARH</option>
+            <option>DELHI</option>
+            <option>GOA</option>
+            <option>GUJARAT</option>
+            <option>HARYANA</option>
+            <option>KARNATAKA</option>
+          </select>
+        </div>
+        <div className="form-group col-4">
+          <label>COUNTRY</label>
+          <input className="form-control" id="country"></input>
+        </div>
       </div>
-      <div className="form-group">
-        <label> Mobile</label>
-        <input
-          className="form-control"
-          id="mobile"
-          placeholder="Enter phone number"
-          type="tel"
-        ></input>
-      </div>
-      <div className="form-group">
-        <label>Aadhar Card Number</label>
-        <input className="form-control" id="aadharnum"></input>
-      </div>
-      <div className="form-group">
-        <label>Price</label>
-        <input className="form-control" id="price" type="number"></input>
-      </div>
-      <div className="form-group">
-        <label>City</label>
-        <input className="form-control" id="city"></input>
-      </div>      
-      <div className="form-group">
-        <label>State</label>
-        <select className="form-control" id="state">
-          <option disabled="true">---Select State----</option>
-          <option>ANDHRA PRADESH</option>
-          <option>ASSAM</option>
-          <option>BIHAR</option>
-          <option>CHANDIGARH</option>
-          <option>DELHI</option>
-          <option>GOA</option>
-          <option>GUJARAT</option>
-          <option>HARYANA</option>
-          <option>KARNATAKA</option>
-        </select>
-      </div>
-      <div className="form-group">
-        <label>Country</label>
-        <input className="form-control" id="country"></input>
-      </div> 
-      <div className="form-group">
-        <label>Gender</label>
-        <select className="form-control" id="gender">
-          <option>Male</option>
-          <option>Female</option>
-        </select>
+      <div className="row">
+        <div className="form-group col-4">
+          <label>GENDER</label>
+          <select className="form-control" id="gender">
+            <option>Male</option>
+            <option>Female</option>
+          </select>
+        </div>
       </div>
       <div className="form-group w-25 mt-2">
         <input
