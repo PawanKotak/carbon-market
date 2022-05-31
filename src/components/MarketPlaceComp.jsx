7213413e5f4d66bf253af04b52b6aca431804728
Pawan Kotak <kotakpawan@gmail.com>
2022-05-31 14:18:16 +0530

    Search button added

--- src/components/MarketPlaceComp.jsx
+++ src/components/MarketPlaceComp.jsx
@@ -1,7 +1,13 @@
-import { Card, Button, ListGroup, ListGroupItem, Modal } from "react-bootstrap";
+import {
+  Card,
+  Button,
+  ListGroup,
+  ListGroupItem,
+  Modal,
+  Search,
+} from "react-bootstrap";
 import { React, useEffect, useState } from "react";
 import axios from "axios";
-
 
 const BACKEND_URL = "http://localhost:4000";
 const config = {
@@ -10,12 +16,14 @@ const config = {
 
 const MaketPlaceComp = () => {
   const [prodDetails, setProdDetails] = useState([]);
+  const [allProdDetails, setAllProdDetails] = useState([]);
   const [selectedProduct, setSelectedProduct] = useState(null);
   const [selectTokenID, setSelectedTokenID] = useState(-1);
   const [amountEth, selectAmountEth] = useState(0);
   const [show, setShow] = useState(false);
   const [addressTo, setAddressTo] = useState(null);
   const [privateKey, setPrivateKey] = useState(null);
+  const [searchKey, setSearchKey] = useState(null);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
@@ -24,6 +32,7 @@ const MaketPlaceComp = () => {
       const result = await axios.get("http://localhost:4000/marketnft");
       console.log(result);
       setProdDetails(result?.data?.data);
+      setAllProdDetails(result?.data?.data);
     })();
   }, []);
 
@@ -36,14 +45,13 @@ const MaketPlaceComp = () => {
   };
 
   const purchaseProduct = async () => {
+    console.log("Number to HEx");
 
-    console.log('Number to HEx');
-    
     let formdata = {
       tokenID: selectTokenID,
       amount: amountEth,
       addressto: addressTo,
-      privatekey: privateKey
+      privatekey: privateKey,
     };
     const result = await axios.post(
       BACKEND_URL + "/buyproduct",
@@ -53,8 +61,41 @@ const MaketPlaceComp = () => {
     handleClose();
   };
 
+  const searchHandle = () => {
+    if (searchKey) {
+      const filterItem = prodDetails.filter(
+        (item) =>
+          item.attributes[2]?.value?.toLowerCase() ==
+            searchKey?.toLowerCase() ||
+          item.attributes[5]?.value?.toLowerCase() == searchKey?.toLowerCase()
+      );
+
+      console.log("filter Item", filterItem);
+      setProdDetails(filterItem);
+    }
+    else
+      setProdDetails(allProdDetails);
+  };
   return (
     <div className="row p-3">
+      <div className="col-md-12 form-horizontal">
+        <div class="input-group" style={{ width: "50%" }}>
+          <input
+            type={"search"}
+            className="form-control"
+            onChange={(e) => setSearchKey(`${e.target.value}`)}
+          ></input>
+          <span class="input-group-btn bg-secondary">
+            <button
+              class="btn btn-default text-white"
+              type="button"
+              onClick={searchHandle}
+            >
+              SEARCH (Place/Carbon Credit)
+            </button>
+          </span>
+        </div>
+      </div>
       {prodDetails &&
         prodDetails.map((item, index) => (
           <Card
